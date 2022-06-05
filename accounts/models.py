808dd6caa5dd8282638e70523ac3c20f04ce763b
fateme-zka <fatemezahrakalantarii@gmail.com>
2022-06-05 16:52:38 +0430

    Adding fields to the models

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    address = models.TextField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    GENDER_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
--- charities/models.py
+++ charities/models.py
@@ -1,13 +1,44 @@
 from django.db import models
+from accounts.models import User
 
 
 class Benefactor(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    EXPERIENCE_CHOICES = (
+        (0, 'Beginner'),
+        (1, 'Average'),
+        (2, 'Expert')
+    )
+    experience = models.SmallIntegerField(default=0, choices=EXPERIENCE_CHOICES)
+    free_time_per_week = models.PositiveSmallIntegerField(default=0)
 
 
 class Charity(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
 
 
 class Task(models.Model):
-    pass
+    assigned_benefactor = models.ForeignKey(Benefactor, null=True, on_delete=models.SET_NULL)
+    charity = models.ForeignKey(Charity, on_delete=models.CASCADE)
+    age_limit_from = models.IntegerField(blank=True, null=True)
+    age_limit_to = models.IntegerField(blank=True, null=True)
+    date = models.DateTimeField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    GENDER_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male')
+    )
+    gender_limit = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+
+    STATE_CHOICES = (
+        ('P', 'Pending'),
+        ('W', 'Waiting'),
+        ('A', 'Assigned'),
+        ('D', 'Done')
+    )
+    state = models.CharField(max_length=1, choices=STATE_CHOICES, default="P")
+    title = models.CharField(max_length=60)
